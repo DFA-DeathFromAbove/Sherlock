76e6e066a887dea4bd1fc12b4e57e9e758e5157e
podbornym <podbornym0542@my.uwstout.edu>
2015-05-12 15:39:17 -0500

    Added all interacteables
    Need zoomDesc, hidden attributes

--- Sherlock/Assets/Scripts/Interactable.cs
+++ Sherlock/Assets/Scripts/Interactable.cs
@@ -36,7 +36,7 @@ public class Interactable : MonoBehaviour {
 			objectName = "desk";
 			discovered = false;
 			change = false;
-			desc = "The desk is covered in film and scrpits.";
+			desc = "The desk is covered in film and scripts.";
 			changeDesc = "After pushing the desk aside, you discover a hidden passage.";
 			secret = "Hidden Passage";
 			zoomDesc = "One of the scripts has the name of the movie. It's called 'The Diamond Theif.' Ironic.";
@@ -67,7 +67,7 @@ public class Interactable : MonoBehaviour {
 			objectName = "film reel";
 			discovered = false;
 			change = false;
-			desc = "One of the scenes from 'The Diamond Theif.' If I had a camera, I could take a close look at the film";
+			desc = "One of the scenes from 'The Diamond Thief.' If I had a camera, I could take a close look at the film";
 		}
 		if(name.ToLower () == "painting")
 		{
@@ -82,6 +82,139 @@ public class Interactable : MonoBehaviour {
 			discovered = false;
 			desc = "The passage seems to lead into the courtyard.";
 			zoomDesc = "The passage seems to lead into the courtyard. Nothing else is notable.";
+		}
+		if(name.ToLower () == "safe")
+		{
+			objectName = "safe";
+			discovered = false;
+			change = false;
+			desc = "A safe hidden behind the set. Requires a 3-digit passcode to open.";
+		}
+		if(name.ToLower () == "saw blade")
+		{
+			objectName = "saw blade";
+			discovered = false;
+			change = false;
+			desc = "The murder weapon. It appears to be clean, but fingerprints might still linger.";
+		}
+		if(name.ToLower () == "camera")
+		{
+			objectName = "camera";
+			discovered = false;
+			change = false;
+			desc = "A constant-wind camera. It appears to have some negatives inside the reel";
+		}
+		if(name.ToLower () == "set")
+		{
+			objectName = "set";
+			discovered = false;
+			change = false;
+			desc = "The set resembles a high security bank, with a pedastal for the diamond.";
+		}
+		if(name.ToLower () == "camera stands")
+		{
+			objectName = "camera stands";
+			discovered = false;
+			change = false;
+			desc = "Various cameras are pointed at the set.";
+		}
+		if(name.ToLower () == "negatives")
+		{
+			objectName = "negatives";
+			discovered = false;
+			change = false;
+			desc = "Some undeveloped film. Until it's developed, I have no idea what it's for.";
+		}
+		if(name.ToLower () == "costumes")
+		{
+			objectName = "costumes";
+			discovered = false;
+			change = false;
+			desc = "Used by the actors and actresses of the film. Full of various costumes.";
+		}
+		if(name.ToLower () == "boot print")
+		{
+			objectName = "boot print";
+			discovered = false;
+			change = false;
+			desc = "This print seems somewhat old, but detailed in the saw dust.";
+		}
+		if(name.ToLower () == "cutting table")
+		{
+			objectName = "cutting table";
+			discovered = false;
+			change = false;
+			desc = "This looks like a table used for measuring and cutting wood, but the saw blade is missing.";
+		}
+		if(name.ToLower () == "tool cabinet")
+		{
+			objectName = "tool cabinet";
+			discovered = false;
+			change = false;
+			desc = "The cabinet is filled with variuos tools, from hammers to wood glue.";
+		}
+		if(name.ToLower () == "props")
+		{
+			objectName = "props";
+			discovered = false;
+			change = false;
+			desc = "Several props, finished and unfinished, are laying about. There's extreme detail in the work.";
+		}
+		if(name.ToLower () == "projector")
+		{
+			objectName = "projector ";
+			discovered = false;
+			change = false;
+			desc = "This projector is most likely used to test the film, and ensure that the movie looks like what the director has in mind.";
+		}
+		if(name.ToLower () == "screen")
+		{
+			objectName = "screen";
+			discovered = false;
+			change = false;
+			desc = "The screen is blank, since the projector isn't running anything right now.";
+		}
+		if(name.ToLower () == "film canisters")
+		{
+			objectName = "film canisters";
+			discovered = false;
+			change = false;
+			desc = "Various film canisters lie about, each with various labels.";
+		}
+		if(name.ToLower () == "ash tray")
+		{
+			objectName = "ash tray";
+			discovered = false;
+			change = false;
+			desc = "The ash tray is cold, and hasn't been used recently. There's a used cigar resting in it.";
+		}
+		if(name.ToLower () == "chemical wash")
+		{
+			objectName = "chemical wash";
+			discovered = false;
+			change = false;
+			desc = "This is a chemical wash used for developing negatives. The room has to be kept dark, otherwise the film is ruined.";
+		}
+		if(name.ToLower () == "costume rack")
+		{
+			objectName = "costume rack";
+			discovered = false;
+			change = false;
+			desc = "The rack has various costume, from ball gowns to a thief outfit.";
+		}
+		if(name.ToLower () == "makeup tables")
+		{
+			objectName = "makeup tables";
+			discovered = false;
+			change = false;
+			desc = "Various kinds of makeup are strewn across the tables.";
+		}
+		if(name.ToLower () == "mirrors")
+		{
+			objectName = "mirrors";
+			discovered = false;
+			change = false;
+			desc = "The mirrors are lit up, allowing the actors to get a better view.";
 		}
 		if (name.ToLower () == "magnifying glass" || name.ToLower() == "magnifying") 
 		{
